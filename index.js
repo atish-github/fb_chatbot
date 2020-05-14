f6f5b6f6ef6a8c5b16913af47eff5c5381c9c985
atish-github <61967033+atish-github@users.noreply.github.com>
2020-05-14 11:29:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var author = 'Atish Sharma'
+console.log('Author : ' + author)
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+// Set port
+app.set('port', (process.env.PORT || 5000))
+// allows us to process data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', function(req, res){
+    res.send('Chatbot root path')
+})
+
+// Route for facebook
+app.get('/webhook/', function(req,res){
+    if(req.query['hub.verify_token'] === 'blondiebytes')
+        res.send(req.query['hub.challenge'])
+    res.send('Wrong Token')
+})
+
+app.listen(app.get('port'), function() {
+    console.log("running app")
+})
